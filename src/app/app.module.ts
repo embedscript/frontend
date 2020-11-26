ac74185aa4faa35fa906c70f7af86dc039f9a236
Janos Dobronszki <dobronszki@gmail.com>
2020-11-26 15:02:41 +0100

    Looks changes (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ListComponent } from './list/list.component';
-import { SingleComponent } from './single/single.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MarkdownModule } from 'ngx-markdown';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ListComponent } from "./list/list.component";
+import { SingleComponent } from "./single/single.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MarkdownModule } from "ngx-markdown";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { HeaderComponent } from "./header/header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [AppComponent, ListComponent, SingleComponent],
+  declarations: [AppComponent, ListComponent, SingleComponent, HeaderComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -22,7 +26,10 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
+    FormsModule,
     FlexLayoutModule,
+    MatToolbarModule,
     MarkdownModule.forRoot(),
     HttpClientModule,
     MarkdownModule.forRoot({ loader: HttpClient }),
