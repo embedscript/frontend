be80c1663177249962c209fa99972e8469d6875a
Janos Dobronszki <dobronszki@gmail.com>
2021-02-16 13:48:18 +0000

    Remove header (#10)

--- src/app/gist-edit/gist-edit.component.ts
+++ src/app/gist-edit/gist-edit.component.ts
@@ -22,32 +22,108 @@ export class GistEditComponent implements OnInit {
   // monaco editor not snapping back to flexlayout defined size
   // https://github.com/microsoft/monaco-editor/issues/28
   tsEditorOptions = {
-    theme: 'vs-dark',
     language: 'html',
     automaticLayout: true,
+    theme: 'vs-light',
+    folding: false,
+    glyphMargin: false,
+    //lineNumbers: false,
+    lineDecorationsWidth: 0,
+    lineNumbersMinChars: 0,
+    renderLineHighlight: false,
+    renderIndentGuides: false,
+    minimap: {
+      enabled: false,
+    },
+    scrollbar: {
+      vertical: 'hidden',
+      horizontal: 'hidden',
+    },
   };
-  tsCode: string = `<script>
-	document.addEventListener("DOMContentLoaded", function (event) {
-    document.getElementById("btn").onclick = function () {
-        Micro.post(
-            "helloworld/call",
-            "backend",
-            {
-                name: "Joe",
-            },
-            function (data) {
-                alert(JSON.stringify(data))
-            }
-        );
-    }
-});
-</script>
-<button id="btn">Call Helloworld as Joe</button>
-<style>
-	#btn {
-		background-color: #fafafa;
-	}
-</style>`;
+  tsCode: string = `<html>
+
+  <head>
+    <style>
+      @import url('https://fonts.googleapis.com/css? 
+  family=Roboto:300, 300i, 400, 400i');
+    </style>
+  
+    <style>
+      html,
+      body {
+        font-family: 'Roboto', sans-serif;
+      }
+  
+      #btn {
+        background-color: #fafafa;
+        border: 1px solid #444;
+        border-radius: 5px;
+        padding: 1em;
+      }
+  
+      h1 {
+        display: inline-block;
+        padding: 0.5em;
+        font-weight: normal;
+        background: #444444;
+        color: #fcfcfc;
+        border-radius: 5px;
+        font-size: 3em;
+      }
+  
+      h2 {
+        font-weight: normal;
+      }
+  
+      #container {
+        text-align: center;
+        padding-top: 3em;
+      }
+    </style>
+  </head>
+  
+  <body>
+    <script>
+      document.addEventListener("DOMContentLoaded", function (event) {
+                document.getElementById("btn").onclick = function () {
+                    Micro.post(
+                    "helloworld/call",
+                        "backend",
+                        {
+                            name: "Joe",
+                        },
+                        function (data) {
+                            alert(JSON.stringify(data))
+                        }
+                    );
+                }
+            });
+    </script>
+  
+    <div id="container">
+      <div>
+        <h1>
+          Embed scripts on any site
+        </h1>
+        <div>
+          <h2>
+            Have access to a wide range of APIs like a database, users service and
+            more.
+          </h2>
+        </div>
+        <div>
+          <h2>
+            You will never neeed a backend again.
+          </h2>
+        </div>
+      </div>
+      <div>
+        <button id="btn">Click to call Helloworld service</button>
+      </div>
+    </div>
+  </body>
+  
+  </html>`;
   tsCodeRendered: string = ``;
 
   //htmlEditorOptions = { theme: 'vs-dark', language: 'html' };
@@ -83,7 +159,7 @@ export class GistEditComponent implements OnInit {
           fs[0].forEach((f) => {
             if (f.path.includes('main')) {
               this.tsCode = f.file_contents;
-              this.render()
+              this.render();
             }
             //if (f.path.includes('index')) {
             //  this.htmlCode = f.file_contents;
@@ -125,6 +201,6 @@ export class GistEditComponent implements OnInit {
   }
 
   render() {
-    this.tsCodeRendered = this.tsCode
+    this.tsCodeRendered = this.tsCode;
   }
 }
