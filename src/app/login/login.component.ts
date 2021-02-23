488661b8ecd33e70e2c7fa29c95df8e43768e39b
Janos Dobronszki <dobronszki@gmail.com>
2021-02-23 15:45:10 +0000

    FIx login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,45 +1,39 @@
-import { Component, OnInit, HostListener } from "@angular/core";
-import { UserService } from "../user.service";
-import { environment } from "../../environments/environment";
-import { Router } from "@angular/router";
+import { Component, OnInit, HostListener } from '@angular/core';
+import { UserService } from '../user.service';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  email: string = "";
-  password: string = "";
-  namespace: string = "micro";
+  email: string = '';
+  password: string = '';
+  namespace: string = 'backend';
 
   constructor(
     private us: UserService,
     private router: Router,
-    private toastr: ToastrService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit() {
-    if (this.us.refreshToken() != "") {
-      this.router.navigate(["/services"]);
-      return;
-    }
-    this.namespace = this.us.namespace();
   }
 
   public githubLogin(event: any) {
     this.us.logout();
-    document.location.href = environment.apiUrl + "/signup/githubLogin";
+    document.location.href = environment.apiUrl + '/signup/githubLogin';
     return false;
   }
 
   public login() {
-    
     this.us
       .login(this.email, this.password, this.namespace)
       .then(() => {
-        document.location.href = "/services";
+        document.location.href = '/';
       })
       .catch((e) => {
         console.log(e);
