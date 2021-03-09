2e7d8e33cd7f3ab8a04c81822d9d1b915d79085e
Janos Dobronszki <dobronszki@gmail.com>
2021-03-09 16:43:53 +0000

    Newl (#28)
    * Fixing editing
    
    * F
    
    * F

--- src/app/render/render.component.ts
+++ src/app/render/render.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { DomSanitizer, SafeHtml, SafeUrl } from '@angular/platform-browser';
 import { UserService } from '../user.service';
 
@@ -9,6 +16,7 @@ import { UserService } from '../user.service';
 })
 export class RenderComponent implements OnInit, OnChanges {
   @Input() name: string;
+  @ViewChild('theiframe', { static: false }) theiframe: ElementRef;
 
   code: SafeHtml;
   iframe: SafeUrl;
@@ -20,6 +28,17 @@ export class RenderComponent implements OnInit, OnChanges {
     this.us.isUserLoggedIn.subscribe((v) => {
       if (v) {
         this.iframe = this.iframeURL();
+        //setInterval(function () {
+        //  (document.getElementById(
+        //    'theiframe'
+        //  ) as any).contentWindow.console.addEventListener(
+        //    'log',
+        //    function (value) {
+        //      alert(value);
+        //      console.log.apply(null, value);
+        //    }
+        //  );
+        //}, 500);
       }
     });
   }
@@ -35,3 +54,26 @@ export class RenderComponent implements OnInit, OnChanges {
     );
   }
 }
+
+var console = {
+  __on: {},
+  addEventListener: function (name, callback) {
+    this.__on[name] = (this.__on[name] || []).concat(callback);
+    return this;
+  },
+  dispatchEvent: function (name, value) {
+    this.__on[name] = this.__on[name] || [];
+    for (var i = 0, n = this.__on[name].length; i < n; i++) {
+      this.__on[name][i].call(this, value);
+    }
+    return this;
+  },
+  log: function () {
+    var a = [];
+    // For V8 optimization
+    for (var i = 0, n = arguments.length; i < n; i++) {
+      a.push(arguments[i]);
+    }
+    this.dispatchEvent('log', a);
+  },
+};
