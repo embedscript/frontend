a282abf30d1493f6825b82011690f4c7921b39f7
Janos Dobronszki <dobronszki@gmail.com>
2021-03-17 08:40:57 +0000

    Script embed fix

--- src/e.js
+++ src/e.js
@@ -1,31 +1,11 @@
-function parse_query_string(query) {
-  var vars = query.split('&');
-  var query_string = {};
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-    var key = decodeURIComponent(pair[0]);
-    var value = decodeURIComponent(pair[1]);
-    // If first entry with this name
-    if (typeof query_string[key] === 'undefined') {
-      query_string[key] = decodeURIComponent(value);
-      // If second entry with this name
-    } else if (typeof query_string[key] === 'string') {
-      var arr = [query_string[key], decodeURIComponent(value)];
-      query_string[key] = arr;
-      // If third or later entry with this name
-    } else {
-      query_string[key].push(decodeURIComponent(value));
-    }
-  }
-  return query_string;
-}
-
-var query = window.location.search.substring(1);
-var qs = parse_query_string(query);
+const script = document.currentScript.getAttribute('script');
+const project = document.currentScript.getAttribute('project');
 
 document.currentScript.outerHTML =
-  '<iframe src="https://backend.m3o.dev/v1/serve?script=' +
-  q['script'] +
+  '<iframe id="iframe-' +
+  script +
+  '"" src="https://backend.m3o.dev/v1/serve?script=' +
+  script +
   '&project=' +
-  q['project'] +
+  project +
   '"></iframe>';
